e65603b822637e0d50706d9a1083ec43dc55b29b
SahilSharma2911 <sahilsharmarksh@gmail.com>
2025-01-17 17:53:36 +0530

    footer fix

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" bg-[#FFD200] py-6 border-t-4 border-black mt-16">
-      <div className=" w-11/12 mx-auto flex items-center justify-between">
+    <div className=" bg-[#FFD200] py-2 border-t-[6px] border-black mt-16">
+      <div className=" w-11/12 mx-auto flex-col lg:flex-row flex gap-5 items-center justify-between">
         {/* logo section  */}
-        <div className=" md:w-[250px]  md:h-[250px] size-40">
+        <div className=" md:w-[250px] md:h-[250px] size-40">
           <Image
             src={"/Images/hero-logo.png"}
             width={250}
@@ -17,11 +17,11 @@ const Footer = () => {
         </div>
 
         {/* second section  */}
-        <div className=" hidden sm:block">
-          <h3 className=" font-lost-south sm:text-[1.8rem] md:text-[2.5rem] font-normal">
+        <div className="">
+          <h3 className=" font-lost-south text-3xl md:text-4xl font-normal tracking-wide mb-1 lg:mb-7">
             connect with us
           </h3>
-          <p className=" font-Montserrat font-bold flex gap-4 items-center">
+          <p className=" font-Montserrat font-bold flex gap-4 items-center text-xl">
             <span className="relative inline-block">
               <Image
                 src="/insta1.png"
@@ -36,8 +36,8 @@ const Footer = () => {
         </div>
 
         {/* last section  */}
-        <div className=" flex flex-col items-center">
-          <h3 className=" font-lost-south text-[1.8rem] md:text-[2.5rem] font-normal">
+        <div className=" flex flex-col items-center mt-6 lg:mt-0 mb-12 lg:mb-0">
+          <h3 className=" font-lost-south text-3xl md:text-4xl tracking-wide font-normal mb-1 lg:mb-7">
             presented by
           </h3>
           <div className=" flex gap-2">
